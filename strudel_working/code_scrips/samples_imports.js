a3c5daa1f1f7de9daa6b762feeda4cfb45e7ce73
Alex de Vries <alexthevries@gmail.com>
2025-05-22 22:38:43 +0200

    add header text

---
+++ strudel_working/code_scrips/samples_imports.js
@@ -0,0 +1,11 @@
+
+s("samples:1").slow(8.5).color("green")._punchcard()
+
+// s("samples:2").slow(8)
+
+// s("samples:3").slow(8)
+
+// s("samples:4").slow(8)._punchcard()
+
+// s("samples:5").slow(3.5).color("green")._punchcard()
+
